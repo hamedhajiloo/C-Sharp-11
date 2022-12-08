09ffcdc477f4f3b1b64bdd59c024636f666f606b
hamedhajiloo <hamedhajiloo@hotmail.com>
2022-12-08 14:32:54 +0330

    Add project files.

---
+++ CSharp11.RawString/Program.cs
@@ -0,0 +1,52 @@
+﻿
+var message1 = "This is a \"quoted text\" \n and I go to the new line";
+Console.WriteLine(message1);
+
+System.Console.WriteLine("_________________________");
+
+var message = """
+ This is a "quoted text" 
+ and I go to the new line
+ """;
+
+Console.WriteLine(message);
+
+System.Console.WriteLine("_________________________");
+
+var message2 = """
+{
+    "name": "hamed"
+}
+""";
+
+Console.WriteLine(message2);
+
+Console.WriteLine("_________________________");
+
+var Latitude = 2;
+var Longitude = 2;
+var location = $$"""
+   You are at {{{Longitude}}, {{Latitude}}}
+""";
+Console.WriteLine(location);
+
+
+Console.WriteLine("_________________________");
+
+var message3 = 
+    @"
+    select *
+    from users
+    where id=1
+    ";
+
+Console.WriteLine(message3);
+
+var message4 = 
+    """
+    select *
+    from users
+    where id=1
+    """;
+
+Console.WriteLine(message4);
